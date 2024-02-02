d8129787e5a01bf7a10d6e55eab7a4d6a3fa3ebd
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-02-03 02:07:29 +0500

    Library intalled  and folder

---
+++ src/config/firebaseConfig/firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWt9dhRJhl1q9yjLRJKfxfLQ0XwiTR97Y",
+  authDomain: "learningmanagementsystem-cda5d.firebaseapp.com",
+  projectId: "learningmanagementsystem-cda5d",
+  storageBucket: "learningmanagementsystem-cda5d.appspot.com",
+  messagingSenderId: "157501334617",
+  appId: "1:157501334617:web:1d01cffca4cd3b183bbd48",
+  measurementId: "G-R6CZ30370S"
+};
+
+const app = initializeApp(firebaseConfig);
