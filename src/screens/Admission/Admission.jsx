eac2968338eb44ba00061f1f2c6262d17b5c6aca
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-02-06 17:21:01 +0500

    Admission form added

--- src/screens/Admission/Admission.jsx
+++ src/screens/Admission/Admission.jsx
@@ -1,9 +1,161 @@
-import React from 'react'
+import React from "react";
 
 const Admission = () => {
   return (
-    <div>Admission</div>
-  )
-}
+    <div className="d-flex justify-content-around mt-5">
+      <form className="d-flex justify-content-center w-100">
+        <div className="w-50">
+          <label htmlFor="fullName">
+            Enter Full Name
+            <br />
+            <input
+              type="text"
+              required
+              placeholder="Full Name"
+              style={{
+                width: "400px",
+              }}
+              className="p-2"
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="fullName">
+            Enter Full Name
+            <br />
+            <input
+              type="text"
+              required
+              placeholder="Full Name"
+              style={{
+                width: "400px",
+              }}
+              className="p-2"
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="fullName">
+            Enter Full Name
+            <br />
+            <input
+              type="text"
+              required
+              placeholder="Full Name"
+              style={{
+                width: "400px",
+              }}
+              className="p-2"
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="fullName">
+            Enter Full Name
+            <br />
+            <input
+              type="text"
+              required
+              placeholder="Full Name"
+              style={{
+                width: "400px",
+              }}
+              className="p-2"
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="fullName">
+            Upload Profile Picture
+            <br />
+            <input
+              type="file"
+              required
+              placeholder="Full Name"
+              style={{
+                width: "400px",
+              }}
+              className="p-2"
+            />
+          </label>
+          <br />
+          <button
+            style={{
+              marginTop: "20px",
+              padding: "5px",
+              width: "1040px",
+              color: "white",
+              fontSize: "18px", 
+              border: "none",
+              backgroundColor: "#0D6DB7"
+            }}
+          >
+            Register
+          </button>
+        </div>
+        <div>
+          <label htmlFor="fullName">
+            Enter Full Name
+            <br />
+            <input
+              type="text"
+              required
+              placeholder="Full Name"
+              style={{
+                width: "400px",
+              }}
+              className="p-2"
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="fullName">
+            Enter Full Name
+            <br />
+            <input
+              type="text"
+              required
+              placeholder="Full Name"
+              style={{
+                width: "400px",
+              }}
+              className="p-2"
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="fullName">
+            Enter Full Name
+            <br />
+            <input
+              type="text"
+              required
+              placeholder="Full Name"
+              style={{
+                width: "400px",
+              }}
+              className="p-2"
+            />
+          </label>
+          <br />
+          <br />
+          <label htmlFor="fullName">
+            Enter Full Name
+            <br />
+            <input
+              type="text"
+              required
+              placeholder="Full Name"
+              style={{
+                width: "400px",
+              }}
+              className="p-2"
+            />
+          </label>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Admission
+export default Admission;
